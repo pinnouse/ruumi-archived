caaffbd74699d2b5b454fa60f7e661044e70d5ca
pinnouse <cupofpho@gmail.com>
2020-01-11 20:33:33 -0500

    Remove ws/room management from this backend

--- data_models.go
+++ data_models.go
@@ -12,30 +12,3 @@ type GOGOCategory struct {
 	CatURL   string `json:"catURL"`
 	Episodes int    `json:"episodes"`
 }
-
-//ROOMS
-type Message struct {
-	Id      string `json:"id"`
-	Content string `json:"content"`
-	Sent    uint32 `json:"sent"`
-	Author  User   `json:"author"`
-}
-
-type Video struct {
-	Source string `json:"source"`
-	Seek   uint32 `json:"seek"`
-}
-
-type User struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-
-type Room struct {
-	Id       string    `json:"id"`
-	Owner    User      `json:"owner"`
-	Video    Video     `json:"video"`
-	Users    []User    `json:"users"`
-	Messages []Message `json:"messages"`
-	hub      *Hub
-}
--- main.go
+++ main.go
@@ -22,26 +22,6 @@ func main() {
 		gogoEpisodeHandler(w, r, client)
 	})
 
-	rooms := make(map[string]Room)
-
-	r.HandleFunc("/addRoom", func(w http.ResponseWriter, r *http.Request) {
-		addRoom(rooms, w, r)
-		log.Println(rooms)
-	})
-
-	r.HandleFunc("/room/{roomId}", func(w http.ResponseWriter, r *http.Request) {
-		getRoom(rooms, w, r)
-	})
-
-	r.HandleFunc("/delRoom/{roomId}", func(w http.ResponseWriter, r *http.Request) {
-		delRoom(rooms, w, r)
-	})
-
-	r.HandleFunc("/chat/{roomId}", func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
-		serveWs(rooms[params["roomId"]].hub, w, r)
-	})
-
 	/* Download a Bencoded file
 	torrentD := make(chan []byte)
 	go getTorrentNyaa(db, 1204164, "Boruto I think", torrentD)
